6a8293c8a8c3fa92f582396ca2d55218bf2363ef
zan <zan@infdata.com>
2023-06-23 12:52:38 +0200

    Fixing API url

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //TODO: change this when hosting
   //apiUrl: "https://kje-je-kaj.fly.dev/api",
-  apiUrl: "http://127.0.0.1:3000/api",
+  apiUrl: "https://lj2-skladsce.onrender.com/api",
   production: true
 };
