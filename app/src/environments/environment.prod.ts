de0d2a3e5c4e34fd782eb17314a24dfdca751d6d
zan <zan@infdata.com>
2023-06-28 16:13:24 +0200

    API url fix

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: "https://lj2-skladsce.onrender.com/api",
+  apiUrl: "https://lj2-skladisce.onrender.com/api",
   //apiUrl: "http://localhost:3000/api",
   production: true
 };
